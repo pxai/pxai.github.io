cd2752808e4788d76685744980973fa15e7386a2
pxai <pello_altadill@cuatrovientos.org>
2016-06-19 00:02:44 +0200

    fixed main

--- app/main.js
+++ app/main.js
@@ -1,8 +1,13 @@
 "use strict";
-var app_component_1 = require('./app.component');
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var router_deprecated_1 = require('@angular/router-deprecated');
-platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
-    router_deprecated_1.ROUTER_PROVIDERS
+/*import {AppComponent}     from './app.component';
+import {bootstrap}        from '@angular/platform-browser-dynamic';
+import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
+
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS
 ]);
+*/
+var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
+var app_component_1 = require('./app.component');
+platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -1,7 +1,12 @@
-import {AppComponent}     from './app.component';
+/*import {AppComponent}     from './app.component';
 import {bootstrap}        from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated';
 
 bootstrap(AppComponent, [
   ROUTER_PROVIDERS
 ]);
+*/
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+
+bootstrap(AppComponent);
